17518871ed75e66dc57faa17758f0271b9e9a512
Pranay Bramhne <pranaybramhne@gmail.com>
2023-06-14 11:23:18 +0530

    modular

--- src/main.js
+++ src/main.js
@@ -1,39 +1,13 @@
-import {MongoClient} from "mongodb";
+import { insertMessage, readMessage } from "./mydb.js";
 
-async function insertMessage(jsonDoc){
-    const uri = "mongodb://127.0.0.1:27017";
-    //open the connection
-    const client = new MongoClient(uri);
-
-    const db = client.db("mydb");
-    const message = db.collection("message");
-
-    //let jsonDoc={message:"hello world", to: "praful", from: "pranay"};
-    await message.insertOne(jsonDoc);
-
-    //close the connection
-    await client.close();
-    console.log("record added");
-}
-
-async function readMessage(){
-    const uri = "mongodb://127.0.0.1:27017";
-    const client = new MongoClient(uri);
-
-    //logic of read
-    let db = client.db("mydb");
-    let message = db.collection("message");
-
-    let list = message.find().toArray();
-    await client.close();
-
-    return list;
-}
 
 
 async function main(){
+    //insert
     let jsonDoc={message:"hello universe", to: "praful", from: "pranay"};
-    //await insertRecord(jsonDoc);
+    await insertMessage(jsonDoc);
+
+    //read
     let list = readMessage();
     console.log(list);
 }
---
+++ src/mydb.js
@@ -0,0 +1,33 @@
+import {MongoClient} from "mongodb";
+
+async function insertMessage(jsonDoc){
+    const uri = "mongodb://127.0.0.1:27017";
+    //open the connection
+    const client = new MongoClient(uri);
+
+    const db = client.db("mydb");
+    const message = db.collection("message");
+
+    //let jsonDoc={message:"hello world", to: "praful", from: "pranay"};
+    await message.insertOne(jsonDoc);
+
+    //close the connection
+    await client.close();
+    console.log("record added");
+}
+
+async function readMessage(){
+    const uri = "mongodb://127.0.0.1:27017";
+    const client = new MongoClient(uri);
+
+    //logic of read
+    let db = client.db("mydb");
+    let message = db.collection("message");
+
+    let list = message.find().toArray();
+    await client.close();
+
+    return list;
+}
+
+export { insertMessage,readMessage};
