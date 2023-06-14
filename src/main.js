d46e53afe7e69d19c7aab9ad0f1e923759134f7d
Pranay Bramhne <pranaybramhne@gmail.com>
2023-06-15 01:41:06 +0530

    node mongo express

--- src/main.js
+++ src/main.js
@@ -1,17 +1,22 @@
-//remotely executable
-//network call
-//network address
-//http://localhost:4000/
+import { MongoClient } from "mongodb";
 import express from "express";
-const app = express();
+const app=express();
+async function main(req,res){
+    const uri="mongodb://localhost:27017"
+    const client = new MongoClient(uri);
 
-// function main(req, res){
-//     res.send("hello")
-// }
 
-app.get("/main", (req, res) => {
-    res.send("hello callback");
-} );
+const db=client.db("mydb");
+const messagecoll= db.collection("message");
+
+let inputDocument = {message: "hello node mongo express"};
+await messagecoll.inputone(inputDocument);
+
+await client.close();
+res.send("hello");
+}
+
+app.get("/main");
 
 //http://localhost:4000
 app.listen(4000);
