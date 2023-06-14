d0a346759547b060eab47a162f1549312cf45c6c
Pranay Bramhne <pranaybramhne@gmail.com>
2023-06-14 10:58:41 +0530

    passing data as argument

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import {MongoClient} from "mongodb";
 
-async function insertRecord(){
+async function insertRecord(jsonDoc){
     const uri = "mongodb://127.0.0.1:27017";
     //open the connection
     const client = new MongoClient(uri);
@@ -8,7 +8,7 @@ async function insertRecord(){
     const db = client.db("mydb");
     const message = db.collection("message");
 
-    let jsonDoc={message:"hello world", to: "praful", from: "pranay"};
+    //let jsonDoc={message:"hello world", to: "praful", from: "pranay"};
     await message.insertOne(jsonDoc);
 
     //close the connection
@@ -17,7 +17,8 @@ async function insertRecord(){
 }
 
 async function main(){
-    await insertRecord();
+    let jsonDoc={message:"hello universe", to: "praful", from: "pranay"};
+    await insertRecord(jsonDoc);
 }
 
 main();
