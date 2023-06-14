8d57f4665808913f95306d0766e73f5e75bbff34
Pranay Bramhne <pranaybramhne@gmail.com>
2023-06-14 10:53:34 +0530

    branch 2,separate insert method

--- src/main.js
+++ src/main.js
@@ -1,15 +1,23 @@
 import {MongoClient} from "mongodb";
 
-async function main(){
+async function insertRecord(){
     const uri = "mongodb://127.0.0.1:27017";
+    //open the connection
     const client = new MongoClient(uri);
 
     const db = client.db("mydb");
     const message = db.collection("message");
 
-    const res = await message.insertOne({});
+    let jsonDoc={message:"hello world", to: "praful", from: "pranay"};
+    await message.insertOne(jsonDoc);
+
+    //close the connection
     await client.close();
-    
-    console.log("record added")
+    console.log("record added");
 }
+
+async function main(){
+    await insertRecord();
+}
+
 main();
