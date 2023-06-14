d455254209aa559cedd28b12f3f3120fbc0d67e5
Pranay Bramhne <pranaybramhne@gmail.com>
2023-06-14 15:35:28 +0530

    hello world using express

--- src/main.js
+++ src/main.js
@@ -3,8 +3,17 @@
 //network address
 //http://localhost:4000/
 import express from "express";
-
 const app = express();
+
+function main(req, res) {
+    //request "some message"
+    res. send("hello");
+}
+
+//https://localhost:4000/main
+app.get("/main", main);
+
+//http://localhost:4000
 app.listen(4000);
 
 
