2da47fc5bb447d1289400e072c08b950bffe36a4
Pranay Bramhne <pranaybramhne@gmail.com>
2023-06-14 10:36:27 +0530

    hello node+mongo

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
-console.log("hello");
+import {MongoClient} from "mongodb";
+
+async function main(){
+    const uri = "mongodb://127.0.0.1:27017";
+    const client = new MongoClient(uri);
+
+    const db = client.db("mydb");
+    const message = db.collection("message");
+
+    const res = await message.insertOne({});
+    await client.close();
+    
+    console.log("record added")
+}
+main();
