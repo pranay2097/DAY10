6f134b6115960c8462394b17563bf91c0d409e18
Pranay Bramhne <pranaybramhne@gmail.com>
2023-06-15 09:49:05 +0530

    node mongo express

--- src/main.js
+++ src/main.js
@@ -1,24 +1,38 @@
 import { MongoClient } from "mongodb";
 import express from "express";
-const app=express();
-async function main(req,res){
-    const uri="mongodb://localhost:27017"
+const app = express();
+
+async function main(req, res){
+    const uri = "mongodb://127.0.0.1:27017/";
     const client = new MongoClient(uri);
 
+    const db = client.db("mydb");
+    const messagecoll = db.collection("message");
 
-const db=client.db("mydb");
-const messagecoll= db.collection("message");
+    let message = req.query.message;
+    let inputDocument = { message: message };
+    await messagecoll.insertOne(inputDocument);
 
-let inputDocument = {message: "hello node mongo express"};
-await messagecoll.inputone(inputDocument);
+    await client.close();
 
-await client.close();
-res.send("hello");
+    res.send("record added");
 }
 
-app.get("/main");
+async function findAllMessage(req,res){
+    const uri = "mongodb://127.0.0.1:4000/";
+    const client = new MongoClient(uri);
+
+    const db = client.db("mydb");
+    const messagecoll = db.collection("message");
+
+    let list = await messagecoll.find().toArray();
+
+    await client.close();
+    res.send(list);
+}
+
+app.get("/main",main);
+app.get("/find", findAllMessage);
 
 //http://localhost:4000
 app.listen(4000);
-
-
